f8f58c5a3c21e1b2cd207e65c740a6744a4fcaed
braydenwerner <braydenwerner0@gmail.com>
2022-03-15 16:46:54 -0700

    Publish to chrome store

---
+++ background-script.js
@@ -0,0 +1,9 @@
+const img = document.createElement('img')
+img.src = 'https://i.imgur.com/CBfPVSM.gif'
+img.style.position = 'fixed'
+img.style.zIndex = 999999
+img.style.bottom = '-5px'
+img.style.right = 0
+img.style.width = '200px'
+img.style.height = '194px'
+document.body.appendChild(img)
